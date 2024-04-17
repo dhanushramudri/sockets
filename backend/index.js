73f8bebfa7e4ecfa5881c43f7be60d5a991dc40e
dhanushramudri <ramudridhanush@gmail.com>
2024-04-17 12:29:46 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -3,38 +3,38 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "http://localhost:3000", // Replace with your client-side origin
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
-
 const PORT = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Server is Running");
+});
+
+http.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 io.on("connection", (socket) => {
-  console.log("New connection");
-
-  socket.emit("message", "Welcome to the chat"); // Initial welcome message
+  console.log("New User Connected");
+  socket.emit("message", "Welcome to the chat");
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
 
-  socket.on("join", ({ room, name }) => {
-    console.log("join", room, name);
+  socket.on("join", ({ name, room }) => {
+    console.log("join", name, room);
+
     socket.join(room);
-    io.to(room).emit("notification", `${name} has joined the ${room}`);
+
+    io.to(room).emit("notification", `${name} has joined ${room}`);
   });
 
   socket.on("messageRoom", ({ room, message }) => {
     console.log("message", room, message);
-    io.to(room).emit("message", message); // Broadcast message to room
+    io.to(room).emit("message", message);
   });
-});
-
-http.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
